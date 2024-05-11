508cb8347fe1b9054678759a943145dd4542d7cc
KURAMA <gonzalezarrieraalejandro@gmail.com>
2024-05-11 17:20:41 -0400

    campo de prueba

---
+++ src/asd/dddd.java
@@ -0,0 +1,29 @@
+package asd;
+
+import java.util.Scanner;
+
+public class dddd {
+
+	public static void main(String[] args) {
+		 String name = "Tommy";
+
+		  int age = 52;
+
+		  if (name == "Tommy") {
+
+		    System.out.println("Hi, Tommy!");
+
+		  } else if (age == 52) {
+
+		    System.out.println("52 years old.");
+
+		  }
+
+		  System.out.println("Out of conditional.");
+	
+	
+	}
+}
+
+
+
